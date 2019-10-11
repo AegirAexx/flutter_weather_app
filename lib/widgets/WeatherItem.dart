f4356fa4cbcecdf0238340fdc3c42b5fda971246
Ægir Æxx <aegir.tomasson@gmail.com>
2019-10-11 00:13:33 +0000

    Changed the card layout

--- lib/widgets/WeatherItem.dart
+++ lib/widgets/WeatherItem.dart
@@ -13,19 +13,36 @@ class WeatherItem extends StatelessWidget {
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text(weather.name, style: new TextStyle(color: Colors.black)),
-            Text(weather.main, style: new TextStyle(color: Colors.black, fontSize: 24.0)),
-            Text(weather.description, style: new TextStyle(color: Colors.black, fontSize: 12.0)),
-            Text('${weather.temp.floor().toString()}°C',  style: new TextStyle(color: Colors.black)),
             Image.network('https://openweathermap.org/img/w/${weather.icon}.png'),
-            Text(new DateFormat.yMMMd().format(weather.date), style: new TextStyle(color: Colors.black)),
-            Text(new DateFormat.Hm().format(weather.date), style: new TextStyle(color: Colors.black)),
+            Text(weather.getWeekdayName, style: new TextStyle(color: Colors.black, fontSize: 24.0)),
+            Text('${weather.getTemp}°C', style: new TextStyle(color: Colors.black, fontSize: 24.0)),
+            Text('  Hi   -    Lo\n${weather.getTempHiLo}', style: new TextStyle(color: Colors.black, fontSize: 10.0)),
           ],
         ),
       ),
     );
   }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Card(
+  //     child: Padding(
+  //       padding: const EdgeInsets.all(8.0),
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: <Widget>[
+  //           Text(weather.name, style: new TextStyle(color: Colors.black)),
+  //           Text(weather.main, style: new TextStyle(color: Colors.black, fontSize: 24.0)),
+  //           Text(weather.description, style: new TextStyle(color: Colors.black, fontSize: 12.0)),
+  //           Text('${weather.temp.floor().toString()}°C',  style: new TextStyle(color: Colors.black)),
+  //           Image.network('https://openweathermap.org/img/w/${weather.icon}.png'),
+  //           Text(new DateFormat.yMMMd().format(weather.date), style: new TextStyle(color: Colors.black)),
+  //           Text(new DateFormat.Hm().format(weather.date), style: new TextStyle(color: Colors.black)),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
